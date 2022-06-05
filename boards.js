ac8cdaa86a8650fd8b76b992bf8200cf9a00895c
aghosh0605 <aghosh0605@gmail.com>
2022-06-05 17:29:32 +0530

    🐛 Fixed POST Response

--- boards.js
+++ boards.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const db = require("./dbQueries/connectDb");
 const writeData = require("./dbQueries/writeDb");
-const readData = require("./dbQueries/readDb");
 const updateData = require("./dbQueries/updateData");
 
 router.get("/", (req, res, next) => {
@@ -12,8 +11,8 @@ router.get("/", (req, res, next) => {
         res.status(400).json({ error: err.message });
         return;
       }
-      readData(db);
       res.status(200).json(rows);
+      next();
     });
   } catch (error) {
     console.log(error);
@@ -24,8 +23,15 @@ router.post("/", async (req, res, next) => {
   try {
     //console.log(req.body);
     writeData(db, req.body.title);
-    res.status(200).json({ message: "success" });
-    next();
+    db.all("SELECT * FROM boards", (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      let last = rows[rows.length - 1];
+      res.status(201).json({ id: last.id, stage: 1, title: req.body.title });
+      next();
+    });
   } catch (error) {
     console.log(error);
   }
