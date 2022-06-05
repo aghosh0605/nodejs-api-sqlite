d74b5512e025a1a995ced01642cbf2352e4e5627
aghosh0605 <aghosh0605@gmail.com>
2022-06-05 16:58:21 +0530

    ✨ PUT Request added

--- boards.js
+++ boards.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require("./dbQueries/connectDb");
 const writeData = require("./dbQueries/writeDb");
 const readData = require("./dbQueries/readDb");
+const updateData = require("./dbQueries/updateData");
 
 router.get("/", (req, res, next) => {
   try {
@@ -21,7 +22,7 @@ router.get("/", (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    console.log(req.body);
+    //console.log(req.body);
     writeData(db, req.body.title);
     res.status(200).json({ message: "success" });
     next();
@@ -30,8 +31,15 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-router.patch("/", (req, res, next) => {
-  console.log(req.body);
+router.put("/:id", (req, res, next) => {
+  try {
+    console.log(req.params.id, req.body.stage);
+    updateData(db, req.params.id, req.body.stage);
+    res.status(200).json({ message: "success" });
+    next();
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
---
+++ dbQueries/updateData.js
@@ -0,0 +1,12 @@
+const runQueries = require("./readDb");
+
+function updateRows(newdb, id, stage) {
+  newdb.run(`UPDATE boards SET stage=? WHERE id=?`, [stage, id], (err) => {
+    if (err) {
+      return console.log("Error in Database edit:  " + err.message);
+    }
+    console.log(`Row was edited to the table`);
+    runQueries(newdb);
+  });
+}
+module.exports = updateRows;
